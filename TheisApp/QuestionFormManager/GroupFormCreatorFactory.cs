f4a2a2d7b837663cadb3f8853dfe74e6d4ce9ad6
Sahar <saharoffice@gmail.com>
2019-11-09 14:27:54 +0200

    Implemented from list for group 2

--- TheisApp/QuestionFormManager/GroupFormCreatorFactory.cs
+++ TheisApp/QuestionFormManager/GroupFormCreatorFactory.cs
@@ -14,15 +14,18 @@ namespace TheisApp.QuestionFormManager
         private static TrailRepository.TrailRepository m_trailRepository = new TrailRepository.TrailRepository();
         public static List<Form> CrateFormList(UserGroup group)
         {
+            var formCreator = new List<Func<TrailRepository.TrailRepository, Form>>();
             switch (group)
             {
                 case UserGroup.One:
-                    var formCreator = new List<Func<TrailRepository.TrailRepository, Form>>()
-                    {
-                        QuestionFormsCreator.CreatePhase1,
-                        QuestionFormsCreator.CreatePhase2
-                    };
+                    formCreator.Add(QuestionFormsCreator.CreatePhase1);
+                    formCreator.Add(QuestionFormsCreator.CreatePhase2);
                     return Create(formCreator);
+
+                case UserGroup.Two:
+                    formCreator.Add(QuestionFormsCreator.CreatePhase2);
+                    return Create(formCreator);
+
                 default: throw new NotImplementedException();
             }
         }
