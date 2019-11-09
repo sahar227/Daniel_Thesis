2b5012a44ef149c2f5f096aab1efdd8c39d2f1fa
Sahar <saharoffice@gmail.com>
2019-11-09 14:13:45 +0200

    Improved form handling architecture

--- TheisApp/QuestionFormManager/GroupFormCreatorFactory.cs
+++ TheisApp/QuestionFormManager/GroupFormCreatorFactory.cs
@@ -11,13 +11,28 @@ namespace TheisApp.QuestionFormManager
 {
     public static class GroupFormCreatorFactory
     {
+        private static TrailRepository.TrailRepository m_trailRepository = new TrailRepository.TrailRepository();
         public static List<Form> CrateFormList(UserGroup group)
         {
             switch (group)
             {
-                case UserGroup.One: return new GroupOneFormCreator().Create();
+                case UserGroup.One:
+                    var formCreator = new List<Func<TrailRepository.TrailRepository, Form>>()
+                    {
+                        QuestionFormsCreator.CreatePhase1,
+                        QuestionFormsCreator.CreatePhase2
+                    };
+                    return Create(formCreator);
                 default: throw new NotImplementedException();
             }
+        }
+
+        private static List<Form> Create(List<Func<TrailRepository.TrailRepository, Form>> formCreators)
+        {
+            var formList = new List<Form>();
+            foreach (var formCreator in formCreators)
+                formList.Add(formCreator(m_trailRepository));
+            return formList;
         }
     }
 }
---
+++ TheisApp/QuestionFormManager/QuestionFormManager.cs
@@ -0,0 +1,41 @@
+﻿using DBModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TheisApp.QuestionFormManager.QuestionFormCreators;
+
+namespace TheisApp.QuestionFormManager
+{
+    public class QuestionFormManager
+    {
+        private readonly List<Form> m_forms = new List<Form>();
+        private int m_currentFormIndex = 0;
+
+        public QuestionFormManager(UserGroup group)
+        {
+            m_forms = GroupFormCreatorFactory.CrateFormList(group);
+        }
+
+        public void Start()
+        {
+            OpenForm(m_forms[m_currentFormIndex]);
+        }
+
+        private void OpenForm(Form f)
+        {
+            //this.Hide();
+            f.Closed += (s, args) => PlayNextForm();
+            f.Show();
+        }
+
+        private void PlayNextForm()
+        {
+            m_currentFormIndex++;
+            if(m_currentFormIndex < m_forms.Count)
+                OpenForm(m_forms[m_currentFormIndex]);
+        }
+    }
+}
